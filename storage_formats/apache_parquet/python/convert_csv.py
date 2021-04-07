df97c426d41f4e01c53cb86ac4fab6c47ab3232a
pawbu <pawbu@users.noreply.github.com>
2021-04-07 14:26:14 +0200

    Add days since epoch column for start and stop dates when converting csv

--- storage_formats/apache_parquet/python/convert_csv.py
+++ storage_formats/apache_parquet/python/convert_csv.py
@@ -1,15 +1,33 @@
 import csv
+from datetime import datetime
 
 source_file = 'accumulated_data_300_million_rows.csv'
 target_file = 'accumulated_data_300_million_rows_converted.csv'
+
+
+def days_since_epoch(date_string: str) -> int:
+    epoch = datetime.utcfromtimestamp(0)
+    date_obj = datetime.strptime(date_string, '%Y%m%d')
+    return (date_obj - epoch).days
+
 
 with open(source_file, newline='') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=';')
     with open(target_file, 'w', newline='') as target_file:
         csv_writer = csv.writer(target_file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
         for row in csv_reader:
-            row[2] = row[2].replace('-', '')
-            row[3] = row[3].replace('-', '')
-            start_year = row[3][:4]
+            start_date: str = row[2]
+            stop_date: str = row[3]
+
+            start_date = start_date.replace('-', '')
+            stop_date = stop_date.replace('-', '')
+            row[2] = start_date
+            row[3] = stop_date
+
+            start_year = start_date[:4]
             row.append(start_year)  # add start_year column to partition on it
+
+            row.append(days_since_epoch(start_date))
+            row.append(days_since_epoch(stop_date))
+
             csv_writer.writerow(row)
